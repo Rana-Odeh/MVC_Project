87e55de939371c4ab719816807e064ae387c546b
Rana-Odeh <rodeh4165@gmail.com>
2026-01-26 20:25:22 +0200

    first commit

---
+++ Project_MVC/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_MVC.Areas.User.Controllers
+{
+    [Area("User")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
