b05bfddbf9f3c9f2c078be7e080d459b992ef970
Rana-Odeh <rodeh4165@gmail.com>
2026-01-26 20:46:35 +0200

    connection to database

---
+++ Project_MVC/data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_MVC.data
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server = DESKTOP-DK2469P; Database=MVC_Project;TrustServerCertificate = True;Trusted_Connection=True");
+        }
+    }
+}
+
--- Project_MVC/obj/Debug/net10.0/Project_MVC.AssemblyInfo.cs
+++ Project_MVC/obj/Debug/net10.0/Project_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87e55de939371c4ab719816807e064ae387c546b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
